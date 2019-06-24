b4cc12cd441a2efc8564730133e8549688c8f442
Wendy Huynh <wendyhuynhs@gmail.com>
2019-06-24 11:09:49 -0700

    Removed Models.js File

--- database/index.js
+++ database/index.js
@@ -2,11 +2,11 @@ const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb://localhost:27017';
 const data = require('./seed.js');
 
-MongoClient.connect(url, {useNewUrlParser:true}, (err, database) => {
+MongoClient.connect(url, { useNewUrlParser: true }, (err, database) => {
   if (err) {
     console.log(`there's an error: ${err}`);
   } else {
-    console.log('mongo connection made')
+    console.log('mongo connection made');
     dbo = database.db('fetch').collection('userInfos');
     dbo.insertMany(data(15), (err,result) => {
       if (err) {
@@ -20,15 +20,15 @@ MongoClient.connect(url, {useNewUrlParser:true}, (err, database) => {
 
 
 const getUserInfo = (req, res) => {
-  let {username, password} = req.body;
-  dbo.findOne({username, password}, (err, data) => {
+  let { username, password } = req.body;
+  dbo.findOne({ username, password }, (err, data) => {
     if (err) {
-      res.status(404).send(err)
+      res.status(404).send(err);
     } else {
-      res.status(200).send(data)
+      res.status(200).send(data);
     }
-  })
-}
+  });
+};
 
 const createUser = (req, res) => {
   let {username, password, email, preferences, matches, images, animalgender, animalbreed, description, location} = req.body;
@@ -39,9 +39,9 @@ const createUser = (req, res) => {
       res.status(200).send('profile created')
     }
   })
-}
+};
 
 module.exports = {
   getUserInfo,
   createUser
-}
+};
