32495f70262fa7a8a6c5a50208c7c39a37efce27
Wendy Huynh <wendyhuynhs@gmail.com>
2019-06-24 16:23:25 -0700

    Updated Router for Put Function

--- server/index.js
+++ server/index.js
@@ -10,8 +10,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api', (req, res)=> Dog.getUserInfo(req, res));
+app.get('/api', (req, res) => Dog.getUserInfo(req, res));
 
 app.post('/api', (req, res) => Dog.createUser(req, res));
-app.put('/api', (req, res) => Dog.createUser(req, res))
-app.listen(port, () => console.log(`listening on port ${port}`))
+app.put('/api', (req, res) => Dog.createUser(req, res));
+app.listen(port, () => console.log(`listening on port ${port}`));
