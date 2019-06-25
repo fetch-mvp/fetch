60af7ad87c211303253aede5a9551dbdbcea2867
Wendy Huynh <wendyhuynhs@gmail.com>
2019-06-25 13:58:43 -0700

    Updated router for put request

--- fetch/server/index.js
+++ fetch/server/index.js
@@ -11,7 +11,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/api', (req, res) => Dog.getUserInfo(req, res));
-// app.get('/api/:id', (req, res) => Dog.getUserInfoID(req, res));
 
 app.post('/api', (req, res) => Dog.createUser(req, res));
 app.put('/api/:_id', (req, res) => Dog.updateUser(req, res));
